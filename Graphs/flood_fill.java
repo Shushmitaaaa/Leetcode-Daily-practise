00a9f93908e65ec96713bd26c8e1833fad6e6168
Shushmita <Shushmitat39@gmail.com>
2026-01-22 07:33:00 +0530

    solved lc733

---
+++ Graphs/flood_fill.java
@@ -0,0 +1,35 @@
+class Solution {
+    private static void dfs(int[][] mat,int sr,int sc,int original,int color,int row,int col){
+        
+        if(sr<0 || sr>=row || sc<0 || sc>=col || mat[sr][sc]!=original){
+            return;
+        }
+        mat[sr][sc]=color;
+        dfs(mat,sr,sc-1,original,color,row,col);
+        dfs(mat,sr,sc+1,original,color,row,col);
+        dfs(mat,sr-1,sc,original,color,row,col);
+        dfs(mat,sr+1,sc,original,color,row,col);
+            
+    }
+
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int row=image.length;
+        int col=image[0].length;
+        int[][] mat=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                mat[i][j]=image[i][j];
+            }
+        }
+        int original=mat[sr][sc];//1
+        if (original == color) return mat;
+        //mat[sr][sc]=color;
+        dfs(mat,sr,sc,original,color,row,col);
+        // dfs(mat,sr,sc+1,original,color,row,col);
+        // dfs(mat,sr-1,sc,original,color,row,col);
+        // dfs(mat,sr+1,sc,original,color,row,col);
+        return mat; 
+
+    }
+}
